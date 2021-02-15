25c091b6a6e80f8daad68b0cd5ee50a4bace2287
Ratcraft <59805596+Ratcraft@users.noreply.github.com>
2021-02-15 16:30:23 +0000

    OOC_Heroes

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tuto
+{
+    class Program
+    {
+        public class heroes
+        {
+            public string Name;
+            public string Ability;
+            public string Origin;
+
+            public heroes(string name,string ability,string origin)
+            {
+                Name = name;
+                Ability = ability;
+                Origin = origin;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            heroes Superman = new heroes("Superman","Super Strenght","Alien");
+            heroes Spiderman = new heroes("Spiderman","Super senses","Human");
+            heroes Wolverine = new heroes("Wolverine","Regenerative Power","Mutant");
+            heroes Flash = new heroes("Flash","Super Speed","Human");
+            heroes Thor  = new heroes("Thor","God of Thunder","Alien");
+        }
+    }
+}
